8f02233ec550dd93da254233efe24daac7d12aeb
numso <dosmun@gmail.com>
2013-11-09 01:03:29 -0700

    aoeu

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(express.cookieParser());
 app.use(express.session(sessOptions));
 app.use(app.router);
 app.use(require('stylus').middleware(__dirname + '/public'));
-app.use(express.static(path.join(__dirname + 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
