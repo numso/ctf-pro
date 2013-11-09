89bf19eabba315bedab067a169e5141906109e51
numso <dosmun@gmail.com>
2013-11-08 21:27:36 -0700

    put map in

--- public/js/game/main.js
+++ public/js/game/main.js
@@ -1,42 +1,113 @@
-/* global PIXI, $, requestAnimationFrame */
+/* global PIXI, $, requestAnimationFrame, TESTMAP */
 'use strict';
 
 window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function (cb) { window.setTimeout(cb, 1000 / 60); };
 
-var inputs = [];
+var assets = ['resources/player.json', 'img/bottom.png', 'img/middle.png'];
+var loader = new PIXI.AssetLoader(assets);
+loader.onComplete = function () {
+  loadGame();
+};
+loader.load();
+
+
+var renderer;
+var stage;
+var map;
 var player;
+var inputs = [];
 
-// You can use either PIXI.WebGLRenderer or PIXI.CanvasRenderer
-var renderer = new PIXI.WebGLRenderer(1400, 600);
+function loadGame() {
+  renderer = new PIXI.autoDetectRenderer(1400, 600);
+  $('#view').after(renderer.view);
+  stage = new PIXI.Stage();
 
-$('#view').after(renderer.view);
+  loadMapTextures();
+  map = createMap();
+  stage.addChild(map);
 
-var stage = new PIXI.Stage();
+  player = createPlayer();
+  stage.addChild(player);
 
-// var bunnyTexture = PIXI.Texture.fromImage('/img/player.png');
-// var bunny = new PIXI.Sprite(bunnyTexture);
+  requestAnimationFrame(animate);
+}
 
-var spriteSheet = new PIXI.SpriteSheetLoader('/resources/player.json');
-spriteSheet.onLoaded = function () {
+function createPlayer() {
   var playerList = [];
   for (var i = 0; i < 3; ++i) {
     playerList.push(new PIXI.Texture.fromFrame(i));
   }
-  player = new PIXI.MovieClip(playerList);
+  var player = new PIXI.MovieClip(playerList);
   player.animationSpeed = 0.2;
-  stage.addChild(player);
-};
+  player.position.x = 500;
+  player.position.y = 200;
+  player.scale.x = 2;
+  player.scale.y = 2;
+  return player;
+}
 
-spriteSheet.load();
+function createMap() {
+  var i, j, id, tempSprite;
 
-requestAnimationFrame(animate);
+  var map = new PIXI.DisplayObjectContainer();
+  for (i = 0; i < TESTMAP.bottom.length; ++i) {
+    for (j = 0; j < TESTMAP.bottom[i].length; ++j) {
+      id = TESTMAP.bottom[i][j];
+      tempSprite = PIXI.Sprite.fromFrame('bot-' + id);
+      tempSprite.position.x = i * 40;
+      tempSprite.position.y = j * 40;
+      map.addChild(tempSprite);
+    }
+  }
+
+  for (i = 0; i < TESTMAP.middle.length; ++i) {
+    for (j = 0; j < TESTMAP.middle[i].length; ++j) {
+      id = TESTMAP.middle[i][j];
+      if (id === null) continue;
+      tempSprite = PIXI.Sprite.fromFrame('mid-' + id);
+      tempSprite.position.x = i * 40;
+      tempSprite.position.y = j * 40;
+      map.addChild(tempSprite);
+    }
+  }
+
+  return map;
+}
+
+function loadMapTextures() {
+  var i, j, tempTexture;
+  var size = 40;
+  var botTexture = new PIXI.Texture.fromImage('img/bottom.png');
+  var midTexture = new PIXI.Texture.fromImage('img/middle.png');
+
+  for (i = 0; i < 8; ++i) {
+    // load bot
+    for (j = 0; j < 6; ++j) {
+      tempTexture = new PIXI.Texture(botTexture, { x: i * size, y: j * size, width: size, height: size });
+      PIXI.TextureCache['bot-' + ((j * 8) + i)] = tempTexture;
+    }
+
+    // load mid
+    for (j = 0; j < 7; ++j) {
+      tempTexture = new PIXI.Texture(midTexture, { x: i * size, y: j * size, width: size, height: size });
+      PIXI.TextureCache['mid-' + ((j * 8) + i)] = tempTexture;
+    }
+  }
+}
 
 function animate() {
   renderer.render(stage);
   requestAnimationFrame(animate);
 
+  var moved = false;
   if (inputs[39]) {
-    player.position.x += 2;
+    moved = true;
+    map.position.x -= 20;
+    player.play();
+  }
+
+  if (inputs[38]) {
+    map.position.y += 20;
     player.play();
   } else {
     player.stop();
