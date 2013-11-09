6cff7cd749f898a1a127b2b04228a6c4dede7827
numso <dosmun@gmail.com>
2013-11-08 20:01:38 -0700

    did stuff

--- public/js/game/main.js
+++ public/js/game/main.js
@@ -3,6 +3,9 @@
 
 window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function (cb) { window.setTimeout(cb, 1000 / 60); };
 
+var inputs = [];
+var test;
+
 // You can use either PIXI.WebGLRenderer or PIXI.CanvasRenderer
 var renderer = new PIXI.WebGLRenderer(1400, 600);
 
@@ -14,10 +17,15 @@ var stage = new PIXI.Stage();
 // var bunny = new PIXI.Sprite(bunnyTexture);
 
 var spriteSheet = new PIXI.SpriteSheetLoader('/resources/player.json');
-spriteSheet.addEventListener('loaded', function(player){
-  var player = new PIXI.Sprite.fromFrame(player.content.json.frames[0].toString());
-  stage.addChild(player);
-});
+spriteSheet.onLoaded = function () {
+  var textures = [];
+  for (var i = 0; i < 3; ++i) {
+    textures.push(new PIXI.Texture.fromFrame(i));
+  }
+  test = new PIXI.MovieClip(textures);
+  test.animationSpeed = 0.2;
+  stage.addChild(test);
+};
 
 spriteSheet.load();
 
@@ -25,6 +33,20 @@ requestAnimationFrame(animate);
 
 function animate() {
   renderer.render(stage);
-
   requestAnimationFrame(animate);
+
+  if (inputs[39]) {
+    test.position.x += 2;
+    test.play();
+  } else {
+    test.stop();
+  }
 }
+
+window.addEventListener('keydown', function (e) {
+  inputs[e.keyCode] = true;
+});
+
+window.addEventListener('keyup', function (e) {
+  inputs[e.keyCode] = false;
+});
