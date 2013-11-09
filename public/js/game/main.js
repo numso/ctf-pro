8f4ab2a52a3c416dfd4e2cb26bae116abfcefb57
numso <dosmun@gmail.com>
2013-11-08 19:36:15 -0700

    did stuff

--- public/js/game/main.js
+++ public/js/game/main.js
@@ -1,16 +1,16 @@
-/* global PIXI, requestAnimationFrame */
+/* global PIXI, $, requestAnimationFrame */
 'use strict';
 
 window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function (cb) { window.setTimeout(cb, 1000 / 60); };
 
 // You can use either PIXI.WebGLRenderer or PIXI.CanvasRenderer
-var renderer = new PIXI.WebGLRenderer(800, 600);
+var renderer = new PIXI.WebGLRenderer(1400, 600);
 
-document.body.appendChild(renderer.view);
+$('#view').after(renderer.view);
 
-var stage = new PIXI.Stage;
+var stage = new PIXI.Stage();
 
-var bunnyTexture = PIXI.Texture.fromImage('bunny.png');
+var bunnyTexture = PIXI.Texture.fromImage('img/top-down_kit.png');
 var bunny = new PIXI.Sprite(bunnyTexture);
 
 bunny.position.x = 400;
