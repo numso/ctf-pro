3e9eff91050fdbc894c80885df5b7fda3396cd24
Richie Preece <richie.preece@gmail.com>
2013-11-08 20:00:51 -0700

    points shots kills

--- io/game_sockets.js
+++ io/game_sockets.js
@@ -7,6 +7,7 @@ module.exports = function (io) {
   io.sockets.on('connection', connect);
 };
 
+var users = {};
 var teams = {
   a: {
     points: 0,
@@ -41,14 +42,21 @@ function connect(socket) {
   var team = assignTeam();
   var user = {
     id: id,
-    team: team
+    team: team,
+    points: 0,
+    kills: 0,
+    shots: 0
   };
   teams[team].users[id] = user;
+  users[id] = user;
 
   socket.emit('conn', {
     team: user.team
   });
-  socket.broadcast.emit('new', user);
+  socket.broadcast.emit('new', {
+    id: user.id,
+    team: user.team
+  });
 
   socket.on('start', function(data){
     if(!data.x) return;
@@ -76,5 +84,35 @@ function connect(socket) {
       x: user.x,
       y: user.y
     });
+  });
+
+  socket.on('point', function(){
+    ++user.points;
+    ++teams[team].points;
+
+    var result = {
+      id: user.id,
+      a: teams.a.points,
+      b: teams.b.points
+    };
+
+    socket.emit('point', result);
+    socket.broadcast.emit('point', result);
+  });
+
+  socket.on('shot', function(data){
+    if(!data.x) return;
+    if(!data.y) return;
+    if(!data.d) return;
+    data.id = user.id;
+
+    ++user.shots;
+
+    socket.broadcast.emit('shot', data);
+  });
+
+  socket.on('kill', function(data){
+    if(!data.id) return;
+    ++users[data.id].kills;
   });
 }
