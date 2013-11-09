3d85748724f8af8c45201b4d0dfcde3bbca3a88a
numso <dosmun@gmail.com>
2013-11-08 18:19:28 -0700

    pixi demo

---
+++ public/js/game/main.js
@@ -0,0 +1,32 @@
+/* global PIXI, requestAnimationFrame */
+'use strict';
+
+window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function (cb) { window.setTimeout(cb, 1000 / 60); };
+
+// You can use either PIXI.WebGLRenderer or PIXI.CanvasRenderer
+var renderer = new PIXI.WebGLRenderer(800, 600);
+
+document.body.appendChild(renderer.view);
+
+var stage = new PIXI.Stage;
+
+var bunnyTexture = PIXI.Texture.fromImage('bunny.png');
+var bunny = new PIXI.Sprite(bunnyTexture);
+
+bunny.position.x = 400;
+bunny.position.y = 300;
+
+bunny.scale.x = 2;
+bunny.scale.y = 2;
+
+stage.addChild(bunny);
+
+requestAnimationFrame(animate);
+
+function animate() {
+  bunny.rotation += 0.01;
+
+  renderer.render(stage);
+
+  requestAnimationFrame(animate);
+}
