5fa5acc75ec32fa9e62840c1bfdcc4d18d8cb0d8
Richie Preece <richie.preece@gmail.com>
2013-11-10 01:12:34 -0700

    a

--- io/game_sockets.js
+++ io/game_sockets.js
@@ -354,7 +354,7 @@ function connect(socket) {
       team: team
     });
 
-    delete flags[team] = undefined;
+    flags[team] = undefined;
 
     alert((user.nickname || user.id) + ' has returned the flag!');
   });
